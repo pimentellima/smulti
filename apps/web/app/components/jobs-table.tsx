4e6a0888e2f1452856fefb881e2d16816b358e36
pimentellima <matheus_dere@hotmail.com>
2025-10-27 18:11:46 -0300

    fix(jobs-table): altera o tamanho da página de 20 para 10 itens

--- apps/web/app/components/jobs-table.tsx
+++ apps/web/app/components/jobs-table.tsx
@@ -65,7 +65,7 @@ export function JobsTable({
         getPaginationRowModel: getPaginationRowModel(),
         initialState: {
             pagination: {
-                pageSize: 20,
+                pageSize: 10,
             },
         },
     })
