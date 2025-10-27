457f9c02a38ab4c50e8355ff3bc385138f35d42d
pimentellima <matheus_dere@hotmail.com>
2025-10-27 12:11:09 -0300

    ref(infra): renomeia stack de implantação para SmultiStack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib'
 import { DeployStack } from '../lib/app-stack'
 
 const app = new cdk.App()
-new DeployStack(app, 'SmultiDeployStack', {
+new DeployStack(app, 'SmultiStack', {
     env: {
         account: '412381757672',
         region: 'us-east-1',
