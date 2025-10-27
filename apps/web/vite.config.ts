f3f72bbe611a28907f6ce766e8ef47567a8dcb35
pimentellima <matheus_dere@hotmail.com>
2025-10-27 12:12:01 -0300

    feat(config): adiciona dotenv como dependência e ajusta configuração do TypeScript

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,11 @@
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: resolve(__dirname, '../../.env') })
 
 export default defineConfig(() => ({
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
